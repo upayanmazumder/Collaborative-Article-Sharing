1aa7eadffe6f2e4d52b44c6ad0f511513608ac40
upayanmazumder <upayanm3@gmail.com>
2024-11-20 11:55:12 +0530

    Add CORS support to Flask app and update requirements

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=["https://api.collaborative-article-sharing.upayan.dev/"])
 
 @app.route('/')
 def home():
