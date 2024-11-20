3fbb83e3795f46ee3f19ebb888c04a80440f1c32
upayanmazumder <upayanm3@gmail.com>
2024-11-20 11:59:50 +0530

    Update CORS configuration to allow localhost and add Docker Compose setup

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["https://api.collaborative-article-sharing.upayan.dev/"])
+CORS(app, origins=["https://api.collaborative-article-sharing.upayan.dev/", "http://localhost:3000/"])
 
 @app.route('/')
 def home():
