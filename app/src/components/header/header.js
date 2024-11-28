ada6ab8a13c1aa6a521ea8be3af79e041280f3c3
upayanmazumder <upayanm3@gmail.com>
2024-11-29 02:52:55 +0530

    gg

--- app/src/components/header/header.js
+++ app/src/components/header/header.js
@@ -1,40 +1,57 @@
 import React from 'react';
 import { FaGithub, FaLink, FaDiscord, FaCloud, FaSignInAlt } from 'react-icons/fa';
 import styles from './header.module.css';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.container}>
-        <a href="/">
-            <div className={styles.logoContainer}>
-            {/* Image is served from '/cas.svg' */}
-            <img src="/cas.svg" alt="CAS Logo" className={styles.icon} />
-            <h1 className={styles.title}>CAS</h1>
-            </div>
-        </a>
+        <Link href="/" className={styles.logoContainer}>
+          <img src="/cas.svg" alt="CAS Logo" className={styles.icon} />
+          <h1 className={styles.title}>CAS</h1>
+        </Link>
         <div className={styles.buttonContainer}>
-          {/* Buttons for links */}
-          <a href="https://github.com/upayanmazumder/Collaborative-Article-Sharing" target="_blank" rel="noopener noreferrer" className={styles.button}>
+          <a
+            href="https://github.com/upayanmazumder/Collaborative-Article-Sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.button}
+          >
             <FaGithub className={styles.iconStyle} />
             GitHub Repo
           </a>
-          <a href="https://api.cas.upayan.dev" target="_blank" rel="noopener noreferrer" className={styles.button}>
-            <FaCloud className={styles.iconStyle} /> {/* FaCloud for API */}
+          <a
+            href="https://api.cas.upayan.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.button}
+          >
+            <FaCloud className={styles.iconStyle} />
             API Link
           </a>
-          <a href="https://pypi.org/project/collaborative-article-sharing/" target="_blank" rel="noopener noreferrer" className={styles.button}>
+          <a
+            href="https://pypi.org/project/collaborative-article-sharing/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.button}
+          >
             <FaLink className={styles.iconStyle} />
             Pypi Repository
           </a>
-          <a href="https://discord.gg/wQTZcXpcaY" target="_blank" rel="noopener noreferrer" className={styles.button}>
+          <a
+            href="https://discord.gg/wQTZcXpcaY"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.button}
+          >
             <FaDiscord className={styles.iconStyle} />
             Discord
           </a>
-          <a href="/auth" className={styles.button}>
+          <Link href="/auth" className={styles.button}>
             <FaSignInAlt className={styles.iconStyle} />
             Authenticate
-          </a>
+          </Link>
         </div>
       </div>
     </header>
