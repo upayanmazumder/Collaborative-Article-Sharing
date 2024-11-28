245bb82f7ac7b1fb0f458ea9e194e871d107907f
upayanmazumder <upayanm3@gmail.com>
2024-11-28 17:35:21 +0530

    ggg

--- api/main.py
+++ api/main.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_cors import CORS
-import os
 
 app = Flask(__name__)
 CORS(app)
@@ -10,7 +9,4 @@ def home():
     return "Wgg"
 
 if __name__ == '__main__':
-    cert_path = '/root/docker/cas/cert.pem'
-    key_path = '/root/docker/cas/key.pem'
-    
-    app.run(debug=True, port=3000, ssl_context=(cert_path, key_path))
+    app.run(debug=True, port=3000)
