88e7070abf5fad3a1d8183e582272ba4d23372c2
Upayan Mazumder <upayanm3@gmail.com>
2024-12-13 21:59:50 +0530

    Add authentication failure and success pages with appropriate messaging

--- app/src/app/auth/connect/failure/page.js
+++ app/src/app/auth/connect/failure/page.js
@@ -5,8 +5,9 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Authentication Success</h1>
-        <p>Congrats on connecting your CLI!</p>
+        <h1>Authentication Failed</h1>
+        <p>Authentication failed as you are not logged in!</p>
+        <a href='/auth'>Login here</a>
       </main>
     </div>
   );
