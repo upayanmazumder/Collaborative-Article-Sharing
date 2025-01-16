74f7cf90fcfdef477d25d1ca3cce40d8738af85c
Upayan Mazumder <upayanm3@gmail.com>
2025-01-16 19:33:16 +0530

    Enhance homepage with installation instructions and code formatting

--- app/src/app/page.js
+++ app/src/app/page.js
@@ -5,6 +5,14 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Welcome to the CAS Homepage</h1>
+        <p>To get started, install the Collaborative Article Sharing package:</p>
+        <pre>
+          <code>pip install collaborative-article-sharing</code>
+        </pre>
+        <p>Then run the following command:</p>
+        <pre>
+          <code>cas</code>
+        </pre>
       </main>
     </div>
   );
